ec37c09d420ecb99d86eb42491bbdda80949ea28
MrDoritos <ianmoore322@gmail.com>
2025-01-31 16:45:27 -0800

    Sensor generation for webpage

---
+++ dev/from_sensors.csv.py
@@ -0,0 +1,102 @@
+#!/bin/python3
+
+import json
+import os
+import sys
+import csv
+from typing import cast
+
+dry_run = True
+sensor_dir = 'sensors/'
+sensor_table = 'sensors.csv'
+sensor_header = 'sensors_header.csv'
+sensor_template = 'sensors_template.json'
+
+def create_data(sensors_csv_file, sensor_template_file, sensor_header_file, sensors_directory):
+    print("directory:", sensors_directory)
+
+    template_json = json.load(sensor_template_file)
+    print("template:", json.dumps(template_json))
+
+    sensors_csv = csv.reader(sensors_csv_file)
+    outline = next(sensors_csv)
+    friendly = next(sensors_csv)
+
+    print(outline)
+
+    header_csv = csv.writer(sys.stdout)
+    #if not dry_run:
+    if True:
+        header_csv = csv.writer(sensor_header_file)
+
+    header_csv.writerow(outline[1:])
+    header_csv.writerow(friendly[1:])
+
+    is_preset = False
+
+    for row in sensors_csv:
+        if row[0] == 'presets':
+            is_preset = True
+            continue
+
+        if not is_preset:
+            continue
+
+        #print(row) 
+
+        sensor_json = json.loads('{}')
+
+        cast_functions = {
+            str: str,
+            int: float,
+            float: float,
+            bool: bool,
+            list: list,
+            dict: dict
+        }
+
+        for i in range(1, len(outline)):
+            #print(outline[i], row[i])
+            data = row[i]
+            key = outline[i]
+            template_data = template_json[key]
+            if not data or len(data) < 1 or data is 0:
+                sensor_json[key] = template_data
+            else:
+                sensor_json[key] = cast_functions[type(template_data)](data)
+                #sensor_json[key] = data
+
+        #print(sensor_json)
+        print(json.dumps(sensor_json))
+
+        sensor_name_fs_safe = sensor_json['sensor-name']
+        sensor_directory = sensors_directory + sensor_name_fs_safe + '/'
+        
+        if not os.path.exists(sensor_directory):
+            if not dry_run:
+                os.makedirs(sensor_directory, exist_ok=True)
+            #print('make directory:', sensor_directory)
+        #print('directory:', sensor_directory)
+
+        sensor_json_file = sensor_directory + sensor_name_fs_safe + '.json'
+
+        if not dry_run:
+            with open(sensor_json_file, mode='w') as sensor_file:
+                json.dump(sensor_json, sensor_file)
+
+        print('write:', sensor_json_file)
+
+
+if not os.path.exists(sensor_table):
+    print('run in directory with "' + sensor_table + '"')
+    sys.exit(-1)
+
+if not os.path.exists(sensor_dir):
+    print("make directory:", sensor_dir)
+    if not dry_run:
+        os.makedirs(sensor_dir, exist_ok=True)
+
+with open(sensor_dir + sensor_template) as template_file:
+    with open(sensor_dir + sensor_header, mode='w') as header_file:
+        with open(sensor_table) as sensor_file:
+            create_data(sensor_file, template_file, header_file, sensor_dir)
