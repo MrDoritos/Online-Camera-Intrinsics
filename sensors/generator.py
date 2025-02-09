5c333d7f9a80084dc647eb1caeb6eba9fd15d828
MrDoritos <ianmoore322@gmail.com>
2025-02-08 19:11:32 -0800

    Implement the generators

--- sensors/generator.py
+++ sensors/generator.py
@@ -4,11 +4,13 @@ import json
 import os
 import sys
 import csv
+import html
 from typing import cast
 
 dry_run = False
 limit_generation = False
 debug_mode = True
+extra_verbosity = False
 
 directory_filepath = 'sensors/'
 all_csv_filename = 'sensors_all.csv'
@@ -24,6 +26,8 @@ image_filetypes = ['bmp', 'png', 'webp', 'jpg', 'jpeg', 'gif']
 #from_sensors.csv.py
 
 def generate_sensors(sensor_fp, template_fp, header_fp, format_fp):
+    print('dissolving', sensor_path)
+
     template_json = json.load(template_fp)
     sensors_csv = csv.reader(sensor_fp)
     outline_csv = next(sensors_csv)
@@ -32,12 +36,17 @@ def generate_sensors(sensor_fp, template_fp, header_fp, format_fp):
     step_csv = next(sensors_csv)
     calc_csv = next(sensors_csv)
 
-    header_csv = csv.writer(sys.stdout)
-    format_csv = csv.writer(sys.stdout)
+    dummy_file = open(os.devnull, mode='w')
+
+    if extra_verbosity:
+        dummy_file = sys.stdout
+
+    header_csv = csv.writer(dummy_file)
+    formats_csv = csv.writer(dummy_file)
 
     if not dry_run:
         header_csv = csv.writer(header_fp)
-        format_csv = csv.writer(format_fp)
+        formats_csv = csv.writer(format_fp)
 
     outline = []
     friendly = []
@@ -48,9 +57,123 @@ def generate_sensors(sensor_fp, template_fp, header_fp, format_fp):
     format_outline = []
     format_csv = []
 
+    for i in range(1, len(outline_csv)):
+        if outline_csv[i] and len(outline_csv[i]):
+            outline.append(outline_csv[i])
+            friendly.append(friendly_csv[i])
+            values.append(values_csv[i])
+            steps.append(step_csv[i])
+            calcs.append(calc_csv[i])
+
+    header_csv.writerow(outline)
+    header_csv.writerow(friendly)
+    header_csv.writerow(values)
+    header_csv.writerow(steps)
+    header_csv.writerow(calcs)
+
+    if debug_mode and extra_verbosity:
+        print('directory:', directory_filepath)
+        print('template:', json.dumps(template_json))
+        print('outline:', outline)
+        print('friendly:', friendly)
+
+    is_preset = False
+
+    if debug_mode:
+        print('reading formats')
+
+    for row in sensors_csv:
+        if debug_mode and extra_verbosity:
+            print(row)
+
+        if row[0] == 'formats':
+            is_preset = True
+            format_outline = [x for x in row[1:] if len(x) > 0]
+            formats_csv.writerow(format_outline)
+            format_csv = row
+            continue
+
+        if row[0] == 'presets':
+            is_preset = True
+            break
+    
+        if not is_preset or len(row[1]) < 1:
+            continue
+
+        formats_csv.writerow([row[format_csv.index(x)] for x in format_outline])
+
+    if debug_mode:
+        print('reading presets')
+
+    for row in sensors_csv:
+        if debug_mode and extra_verbosity:
+            print(row)
+
+        if row[0] == 'presets':
+            is_preset = True
+            continue
+        
+        if not is_preset:
+            continue
+
+        sensor_json = json.loads('{}')
+
+        cast_functions = {
+            str: str,
+            int: float,
+            float: float,
+            bool: bool,
+            list: list,
+            dict: dict
+        }
+
+        for x in template_json:
+            i = outline_csv.index(x)
+            data = row[i]
+            key = x
+            template_data = template_json[key]
+
+            if debug_mode and extra_verbosity:
+                print(outline_csv[i], row[i], template_data, type(template_data))
+
+            if not data or len(data) < 1 or data == 0:
+                sensor_json[key] = template_data
+            else:
+                sensor_json[key] = cast_functions[type(template_data)](data)
+
+        sensor_name_fs_safe = sensor_json['sensor-name']
+        sensor_directory = directory_filepath + sensor_name_fs_safe + '/'
+        sensor_json_file = sensor_directory + sensor_name_fs_safe + '.json'
+
+        if not os.path.exists(sensor_directory):
+            if debug_mode:
+                dry_msg = 'create directory:'
+                if dry_run:
+                    dry_msg = 'create directory (dry):'
+                print(dry_msg, sensor_directory)
+
+            if not dry_run:
+                os.makedirs(sensor_directory, exist_ok=True)
+                
+
+        if debug_mode:
+            dry_msg = 'generating:'
+            if dry_run:
+                dry_msg = 'generating (dry):'
+            print(dry_msg, sensor_json_file)
+
+        if not dry_run:
+            with open(sensor_json_file, mode='w') as sensor_file:
+                json.dump(sensor_json, sensor_file)
+
+        if limit_generation:
+            return
+
 #generate_all.csv.py
 
 def generate_allcsv(all_csv_fp, template_fp, header_fp, cache_fp):
+    print('compile', all_csv_filename, 'from sensor directories')
+
     template_json = json.load(template_fp)
     all_csv = csv.writer(sys.stdout)
     header_csv = csv.reader(header_fp)
@@ -66,12 +189,108 @@ def generate_allcsv(all_csv_fp, template_fp, header_fp, cache_fp):
     all_csv.writerow(header)
     all_csv.writerows(header_csv)
 
+    for _, dirs, _ in os.walk(directory_filepath):
+        for dir in dirs:
+            n_dir = directory_filepath + dir + '/'
+            for _, _, files in os.walk(n_dir):
+                for file in files:
+                    if not file.endswith('.json'):
+                        continue
+
+                    n_file = n_dir + file
+                    
+                    if debug_mode:
+                        print('reading:', n_file)
+
+                    with open(n_file, mode='r') as sensor_json_fp:
+                        sensor_json = json.load(sensor_json_fp)
+
+                        values = [sensor_json[x] for x in header]
+                        
+                        for i in range(len(values)):
+                            if values[i] == 0:
+                                values[i] = ''
+
+                        all_csv.writerow(values)
+                        cache_csv.writerow(values[0:2])
+
+                    if limit_generation:
+                        return
+
 #generate_pages.py
 
 def generate_pages(header_fp, page_fp):
+    print('generate pages using the API header file', header_filename)
+    
     page_data = page_fp.read()
     header_csv = csv.reader(header_fp)
     header = [x for x in header_csv]
+
+    for _, dirs, _ in os.walk(directory_filepath):
+        for dir in dirs:
+            n_dir = directory_filepath + dir + '/'
+            for _, _, files in os.walk(n_dir):
+                json_filepath = None
+                image_filepaths = []
+
+                for file in files:
+                    if file.endswith('.json'):
+                        json_filepath = file
+                    elif file.endswith('.html'):
+                        continue
+                    elif sum(1 for x in image_filetypes if file.endswith(x)):
+                        image_filepaths.append(file)
+
+                if not json_filepath or not len(json_filepath):
+                    continue
+
+                n_file = n_dir + json_filepath
+
+                if debug_mode:
+                    print('reading:', n_file)
+
+                with open(n_file, mode='r') as sensor_json_fp:
+                    sensor_json = json.load(sensor_json_fp)
+
+                    page_tmp = page_data
+
+                    sensor_properties = ''
+                    sensor_images = ''
+
+                    sensor_html_filepath = n_dir + 'index.html'
+
+                    for x in sensor_json:
+                        key_esc = html.escape(str(x))
+                        value_esc = html.escape(str(sensor_json[x]))
+
+                        if x in header[0]:
+                            h_i = header[0].index(x)
+                            key_esc = html.escape(str(header[1][h_i]))
+
+                        page_tmp = page_tmp.replace(f'${x}', html.escape(str(sensor_json[x])))
+
+                        sensor_properties = sensor_properties + \
+                        f'<div><b>{key_esc}:</b><span>{value_esc}</span></div>'
+
+                    for x in image_filepaths:
+                        sensor_images = sensor_images + \
+                        f'<div><img src="{x}" /></div>'
+
+                    page_tmp = page_tmp.replace('$sensor_properties', sensor_properties)
+                    page_tmp = page_tmp.replace('$sensor_images', sensor_images)
+
+                    if debug_mode:
+                        dry_msg = 'writing:'
+                        if dry_run:
+                            dry_msg = 'writing (dry):'
+                        print(dry_msg, sensor_html_filepath)
+
+                    if not dry_run:
+                        with open(sensor_html_filepath, mode='w') as sensor_html_fp:
+                            sensor_html_fp.write(page_tmp)
+
+                    if limit_generation:
+                        return
 
 def run_sensors():
     template_path = directory_filepath + template_filename
@@ -91,9 +310,9 @@ def run_allcsv():
     all_csv_path = directory_filepath + all_csv_filename
 
     with open(template_path, 'r') as template_fp:
-        with open(header_path, 'w') as header_fp:
+        with open(header_path, 'r') as header_fp:
             with open(cache_path, 'w') as cache_fp:
-                with open(all_csv_path, 'r') as all_csv_fp:
+                with open(all_csv_path, 'w') as all_csv_fp:
                     generate_allcsv(all_csv_fp, template_fp, header_fp, cache_fp)
 
 def run_pages():
